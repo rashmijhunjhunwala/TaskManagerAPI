f43286a8492280712a30872871f9e925f1062297
rashmijhunjhunwala <rashmijhunjhunwala068@gmail.com>
2019-07-19 16:41:35 +0530

    first

---
+++ server/models/todo.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var TodoSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+});
+var Todo= mongoose.model('Todo', TodoSchema);
+module.exports = {Todo};
